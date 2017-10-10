ba4576a3513bc8e22a373000af4c32123f004a2e
Frans Huang <franssiswanto@gmail.com>
2017-10-10 19:34:54 +0700

    Updated

--- src/reducers/pages/inputPenerimaanBarangPage.js
+++ src/reducers/pages/inputPenerimaanBarangPage.js
@@ -1,15 +1,16 @@
 const inputPenerimaanBarangPage = (state = {
     isSaving: false,
     isError: false,
+    isSuccess: false,
     errorMessage: ""
 }, action) => {
     switch(action.type){
         case "INPUT_PENERIMAAN_BARANG_SAVING": 
-            return Object.assign({}, state, {isSaving: true, isError: false})
+            return Object.assign({}, state, {isSaving: true, isError: false, isSuccess: false})
         case "INPUT_PENERIMAAN_BARANG_SAVED":
-            return Object.assign({}, state, {isSaving: false, isError: false})
+            return Object.assign({}, state, {isSaving: false, isError: false, isSuccess: true})
         case "INPUT_PENERIMAAN_BARANG_SHOW_ERROR":
-            return Object.assign({}, state, {isSaving: false, isError: true, errorMessage: action.errorMessage})
+            return Object.assign({}, state, {isSaving: false, isError: true, isSuccess: false, errorMessage: action.errorMessage})
         default:
             return state;
     }
